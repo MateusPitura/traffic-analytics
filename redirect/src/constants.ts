6a40e0f19b7d39c68695792862df08e0e4ed0fb0
MateusPitura <mateuspitura@gmail.com>
2026-03-28 17:04:56 -0300

    feat: add mapUtmIdToUrl constant and integrate UTM ID handling in main logic for URL redirection

---
+++ redirect/src/constants.ts
@@ -0,0 +1,5 @@
+export const mapUtmIdToUrl: Record<string, string> = {
+    '03J51X': 'github.com/MateusPitura/app-reactnative-imc',
+    '0IGG1X': 'github.com/MateusPitura/app-reactnative-combustivel',
+    'OKOH1X': 'github.com/MateusPitura/app-reactnative-vestibular'
+}
--- redirect/src/main.ts
+++ redirect/src/main.ts
@@ -1,11 +1,22 @@
 import { collectVisitData } from "@script/collectVisitData";
 import { sendToWorker } from "@script/sendToWorker";
+import { mapUtmIdToUrl } from "./constants";
 
 const visitData = await collectVisitData();
 
 sendToWorker(visitData);
 
 const params = new URLSearchParams(window.location.search);
+
+const utmId = params.get("r");
+
+if (utmId) {
+  const url = mapUtmIdToUrl[utmId];
+
+  if (url) {
+    window.location.replace(`https://${url}`);
+  }
+}
 
 let rawUrl = params.get("q");
 
