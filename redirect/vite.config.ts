2202eff545802f064aec1652807409601f5a6847
MateusPitura <mateuspitura@gmail.com>
2026-03-28 10:16:26 -0300

    feat: added vite plugins to minify html and build all in a single file

---
+++ redirect/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { createHtmlPlugin } from 'vite-plugin-html'
+
+export default defineConfig({
+  build: {
+    modulePreload: false,
+  },
+  plugins: [viteSingleFile(), createHtmlPlugin()],
+});
