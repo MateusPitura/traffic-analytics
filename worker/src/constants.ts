3b8d79adc7b5717dcca4dd98e18b1bccaae2b807
MateusPitura <mateuspitura@gmail.com>
2026-04-02 22:45:13 -0300

    feat: update CORS handling to support multiple allowed origins and improve response headers

--- worker/src/constants.ts
+++ worker/src/constants.ts
@@ -1,8 +1,15 @@
 export const COOKIE_NAME = 'traffic_analytics_cookie_id';
 
+export const allowedOrigins = [
+	'https://mateuspitura.com',
+	'https://url.mateuspitura.com',
+	'https://dms.mateuspitura.com',
+	'https://saas.mateuspitura.com',
+];
+
 export const corsHeaders = {
-	'Access-Control-Allow-Origin': 'https://mateuspitura.com',
 	'Access-Control-Allow-Methods': 'POST, OPTIONS',
 	'Access-Control-Allow-Headers': 'Content-Type',
 	'Access-Control-Allow-Credentials': 'true',
+	Vary: 'Origin',
 };
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,17 +1,27 @@
 import { collectFields } from './collectFields';
-import { COOKIE_NAME, corsHeaders } from './constants';
+import { allowedOrigins, COOKIE_NAME, corsHeaders } from './constants';
 import { sendToFirestore } from './sendToFirestore';
 import { Env } from './types';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		const origin = request.headers.get('Origin');
+		let corsOrigin = '';
+
+		if (origin && allowedOrigins.includes(origin)) {
+			corsOrigin = origin;
+		}
+
 		if (request.method === 'OPTIONS') {
 			return new Response(null, {
 				status: 204,
-				headers: corsHeaders,
+				headers: {
+					'Access-Control-Allow-Origin': corsOrigin,
+					...corsHeaders,
+				},
 			});
 		}
-		
+
 		if (request.method !== 'POST') {
 			return new Response('Method Not Allowed', { status: 405 });
 		}
@@ -21,7 +31,7 @@ export default {
 			return new Response('Not Found', { status: 404 });
 		}
 
-		const {cookieId, fields} = await collectFields(request);
+		const { cookieId, fields } = await collectFields(request);
 
 		ctx.waitUntil(sendToFirestore(env, fields));
 
@@ -29,6 +39,7 @@ export default {
 			status: 204,
 			headers: {
 				'Set-Cookie': `${COOKIE_NAME}=${cookieId}; Path=/; Max-Age=31536000; SameSite=None; Secure`,
+				'Access-Control-Allow-Origin': corsOrigin,
 				...corsHeaders,
 			},
 		});
